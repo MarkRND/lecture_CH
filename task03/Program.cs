37cdcedb23530b11533c47dd9a659ad7c88675f2
Mark <masmik@rambler.ru>
2022-08-09 23:10:58 +0300

    add task03

---
+++ task03/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая будет выдавать названия дня недели по заданому номеру
+Console.Write("Введите число от 1-7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine($"Число {num} => Понедельник");
+}
+if (num == 2)
+{
+    Console.WriteLine($"Число {num} => Вторник");
+}
+if (num == 3)
+{
+    Console.WriteLine($"Число {num} => Среда");
+}
+if (num == 4)
+{
+    Console.WriteLine($"Число {num} => Четверг");
+}
+if (num == 5)
+{
+    Console.WriteLine($"Число {num} => Пятница");
+}
+if (num == 6)
+{
+    Console.WriteLine($"Число {num} => Суббота");
+}
+if (num == 7)
+{
+    Console.WriteLine($"Число {num} => Воскресенье");
+}
+if (num > 7)
+{
+    Console.WriteLine($"Число {num} это число выходит за диапозон от 1-7, ОШИБКА веедите правильное число");
+}
+if (num < 7)
+{
+    Console.WriteLine($"Число {num} это число выходит за диапозон от 1-7, ОШИБКА веедите правильное число");
+}
