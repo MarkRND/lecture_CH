98f38f23b490f4f34e40eb55be5088d2545c9c45
Mark <masmik@rambler.ru>
2022-08-10 00:38:34 +0300

    add task05

---
+++ task05/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу которая
+// 1 на вход принимаетодно число
+// 2 а на выходе показывает все целые числа в промежуток от -N до N
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("вывод чисел ");
+// if (num > 0)
+
+    for (int num2 = -num; num2 < num; num2++)
+    {
+    Console.Write(num2 + " ");
+    }    
+
+// if (num < 0)
+
+//     for (int num2 = -num; num2 > num; num2++)
+// {
+//     Console.Write(num2 + " ");
+// }
