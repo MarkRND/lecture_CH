94a198b46a7e179e600cf2ac69639f73d802298d
Mark <masmik@rambler.ru>
2022-08-14 00:24:17 +0300

    add task16

--- task14/Program.cs
+++ task14/Program.cs
@@ -4,8 +4,8 @@
 
 Console.Write("Введите число : ");
 int number = Convert.ToInt32(Console.ReadLine());
-;
-if (number / 7 == 0 & number / 23 == 0)
+
+if (number / 7 == 0 && number / 23 == 0)
 {
     Console.WriteLine($"Число {number} кратно  числам 7 и 23 ");
 }
---
+++ task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая
+// 1 принимает на вход 2 числа
+// 2 и проверяет, являеться ли одно число квадратом другого
+
+Console.Write("Введите первое число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num % num1 == 0 || num1 % num == 0)
+{
+    Console.WriteLine($"Числа {num} {num1} => ДА");
+}
+else
+{
+    Console.WriteLine($"Числа {num} {num1} => НЕТ ");
+}
