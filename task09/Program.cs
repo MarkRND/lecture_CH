cb82d1ff451e5a82a39c1f016a017cafc1c35b65
Mark <masmik@rambler.ru>
2022-08-13 22:07:40 +0300

    task09+

--- task09/Program.cs
+++ task09/Program.cs
@@ -4,14 +4,31 @@
 
 int number = new Random().Next(10, 100);  // 99+1
 //Console.WriteLine($"Случайное число диапозона 10 - 99 => {number}");
-int firstDigit = number / 10; // получаем первую цифру числа
-int secondDigit = number % 10; // получаем вторую цифру числа
+// int firstDigit = number / 10; // получаем первую цифру числа
+// int secondDigit = number % 10; // получаем вторую цифру числа
 
 // if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} =>  {firstDigit}");
 // else if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
-// else Console.WriteLine($"Наибольшая цифра числа {number} =>  {secondDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа {number} =>  {secondDigit}");              1) метода решения
 
-string result = firstDigit == secondDigit ? "Цифры равны" 
-: firstDigit > secondDigit 
-? firstDigit.ToString() : secondDigit.ToString();  // тернарные операторы ? - тогда, : - иначе
-Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
+
+
+// string result = firstDigit == secondDigit ? "Цифры равны" 
+// : firstDigit > secondDigit 
+// ? firstDigit.ToString() : secondDigit.ToString();  // тернарные операторы ? - тогда, : - иначе
+// Console.WriteLine($"Наибольшая цифра числа {number} => {result}");                            // 2) метод решения
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10; // получаем первую цифру числа
+    int secondDigit = num % 10; // получаем вторую цифру числа
+    if (firstDigit == secondDigit) return 0;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
+
+int result = MaxDigit(number);
+string res = result == 0 ? " цифры равны" : result.ToString();
+// int result2 = MaxDigit(56);
+Console.WriteLine($"Наибольшая цифра числа {number} => {res}");
+// Console.WriteLine($"Наибольшая цифра числа {56} => {result2}");
