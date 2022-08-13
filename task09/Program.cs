eb6cfa24f700fa2ddf908e2bc50bc9e40d9e2250
Mark <masmik@rambler.ru>
2022-08-13 21:25:01 +0300

    add task09

---
+++ task09/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, котора
+// 1.выводиь случайное число из отрезка [10, 99] и
+// 2. показывает наибольшую цифру
+
+int number = new Random().Next(10, 100);  // 99+1
+//Console.WriteLine($"Случайное число диапозона 10 - 99 => {number}");
+int firstDigit = number / 10; // получаем первую цифру числа
+int secondDigit = number % 10; // получаем вторую цифру числа
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} =>  {firstDigit}");
+// else if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
+// else Console.WriteLine($"Наибольшая цифра числа {number} =>  {secondDigit}");
+
+string result = firstDigit == secondDigit ? "Цифры равны" 
+: firstDigit > secondDigit 
+? firstDigit.ToString() : secondDigit.ToString();  // тернарные операторы ? - тогда, : - иначе
+Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
