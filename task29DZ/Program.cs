dbeb1dd53d1e12445b385d09b66b7043c6c2937b
Mark <masmik@rambler.ru>
2022-08-16 21:39:45 +0300

    add task29DZ

---
+++ task29DZ/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая 
+// 1)задаёт массив из 8 элементов 
+// 2)и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] NewArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i <= 7; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+int[] array = NewArray();
+void PrintArray()
+{   
+    Console.Write("Массив из 8 элeментов [ ");
+    for (int i = 0; i <= 7; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+PrintArray();
+NewArray();
