b913b75da97ae6ec2199aba17f46b8a3ecda73d7
Mark <masmik@rambler.ru>
2022-08-16 17:53:45 +0300

    add task24

---
+++ task24/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 24: Напишите программу, которая
+// 1принимает на вход число (А)
+//  2и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write(" Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// for (int i = 0; i <= num; i++)
+// {
+//     sum = sum + i;
+//     // sum += i;
+// }
+// Console.WriteLine($"сумма чисел от 1 до {num} = {sum}" );
+
+
+
+int SumNumbers(int number)
+{
+int sum = 0;
+for (int i = 0; i <= num; i++)
+{
+    sum = sum + i;
+    // sum += i;
+}
+    return sum;
+}
+int result = SumNumbers(num);
+Console.WriteLine($"сумма чисел от 1 до {num} = {result}" );
---
+++ task26/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 26: Напишите программу, 
+// 1)которая принимает на вход число 
+// 2)и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+
+
+
+Console.Write(" Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int x = num;
+int count = 0;
+// if (x == 0)
+//     {
+//         count = 1;
+//     }
+// else
+// {
+// while (x != 0)
+//     {
+//         x = x / 10;
+//         count++;
+//     }
+// }
+
+int Numbers(int number)
+{
+int x = number;
+ if (x == 0)
+    {
+        count = 1;
+    }
+else
+{
+while (x != 0)
+    {
+        x = x / 10;
+        count++;
+    }
+}  
+return count;
+}
+
+Console.WriteLine(Numbers(num));
